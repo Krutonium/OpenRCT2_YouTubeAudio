0fb0f6b3c48066f30d0ae0c5087544ba9e57d5be
Krutonium <PFCKrutonium@gmail.com>
2018-06-25 05:46:00 -0400

    Uncommented some code.

--- OpenRCT2Music_DL.py
+++ OpenRCT2Music_DL.py
@@ -10,9 +10,9 @@ from pytube import YouTube
 
 print('Downloading Video from YouTube...')
 yt = YouTube('https://www.youtube.com/watch?v=ykuT2RqB_LM')
-#yt.streams.first().download()
+yt.streams.first().download()
 print('Download Done, creating tracks...')
-#os.rename('Rollercoaster Tycoon 2 Gameplay Music Remastered - PC OST.mp4','RCT2Music.mp4')
+os.rename('Rollercoaster Tycoon 2 Gameplay Music Remastered - PC OST.mp4','RCT2Music.mp4')
 #At this point we have the raw audio file
 
 clip = mp.VideoFileClip("RCT2Music.mp4").subclip(0,95)

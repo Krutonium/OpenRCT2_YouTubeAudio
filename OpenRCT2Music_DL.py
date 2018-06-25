9b165615450b4dacd77abbe04b7eaa7a9684d8ad
Krutonium <PFCKrutonium@gmail.com>
2018-06-25 18:36:01 -0400

    Add More Music to Write Out, and a Dry Run boolean.

--- OpenRCT2Music_DL.py
+++ OpenRCT2Music_DL.py
@@ -5,11 +5,16 @@ import moviepy.editor as mp
 from pytube import YouTube
 #pytube - https://github.com/nficano/pytube
 
+DryRun = True
+
+
 print('Downloading Video from YouTube...')
 yt = YouTube('https://www.youtube.com/watch?v=ykuT2RqB_LM')
-#yt.streams.first().download()
+if DryRun == False:
+    yt.streams.first().download()
+    os.rename('Rollercoaster Tycoon 2 Gameplay Music Remastered - PC OST.mp4','RCT2Music.mp4')
 print('Download Done, creating tracks...')
-#os.rename('Rollercoaster Tycoon 2 Gameplay Music Remastered - PC OST.mp4','RCT2Music.mp4')
+
 #At this point we have the raw audio file
 
 clip = mp.VideoFileClip("RCT2Music.mp4")
@@ -33,8 +38,20 @@ clip = mp.VideoFileClip("RCT2Music.mp4")
 
 #Unfortunatly some of these have a little extra audio at the beginning/end either added or removed.
 
-clip.subclip(0,96).audio.write_audiofile("RomanFanfare.mp3")
-clip.subclip(96,211).audio.write_audiofile("OrientalStyle.mp3")
-clip.subclip(211,350).audio.write_audiofile("MartianStyle.mp3")
+if not os.path.exists("./wavs/"):
+    os.makedirs("./wavs/")
+if not os.path.exists("./dats/"):
+    os.makedirs("./dats/")
 
-    
+#Math is Previous Time, Previous Time + Duration
+
+clip.subclip(0,96).audio.write_audiofile("./wavs/01RomanFanfare.wav")
+clip.subclip(96,211).audio.write_audiofile("./wavs/02OrientalStyle.wav")
+clip.subclip(211,350).audio.write_audiofile("./wavs/03MartianStyle.wav")
+clip.subclip(350,469).audio.write_audiofile("./wavs/04JungleDrumsStyle.wav")
+clip.subclip(469,579).audio.write_audiofile("./wavs/05ToylandStyle.wav")
+clip.subclip(569,695).audio.write_audiofile("./wavs/06SpaceStyle.wav")
+clip.subclip(695,878).audio.write_audiofile("./wavs/07HorrorStyle.wav")
+clip.subclip(878,998).audio.write_audiofile("./wavs/08TechoStyle.wav") #Seems to end too late?
+clip.subclip(998,1116).audio.write_audiofile("./wavs/09WaterStyle.wav")
+clip.subclip(1116,1232).audio.write_audiofile("./wavs/10WildWestStyle.wav")
